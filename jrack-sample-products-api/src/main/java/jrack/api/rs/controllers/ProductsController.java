4225108b6823f81ab6bc974952661f5d8d12ed38
Marcelo Manzan <manzan@gmail.com>
2014-08-17 05:30:49 -0300

    sample-products: resource entrypoint

--- jrack-sample-products-api/src/main/java/jrack/api/rs/controllers/ProductsController.java
+++ jrack-sample-products-api/src/main/java/jrack/api/rs/controllers/ProductsController.java
@@ -1,7 +1,28 @@
 package jrack.api.rs.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Path("products")
 public class ProductsController {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response entrypoint() throws IOException {
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream("jrack/api/rs/views/products.json");
+		JsonNode entity = new ObjectMapper().readTree(in);
+		ResponseBuilder response = Response.ok(entity);
+		return response.build();
+	}
+
 }
